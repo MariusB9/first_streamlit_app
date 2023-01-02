7fc611a66dbafe5030325a5c085d12fc7a6ec6c2
MariusB9 <121283304+MariusB9@users.noreply.github.com>
2023-01-02 08:26:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('Hello World')
-streamlit.header('Breakfast Menu')
+streamlit.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
